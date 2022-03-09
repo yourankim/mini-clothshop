5103ff865525dd2c427c1e12aa327488d3a2f1f2
yourankim <youran.dev@gmail.com>
2022-03-09 21:51:18 +0900

    Refactoring list filtering process 리스트를 필터링할 때마다데이터를 변경하여  다시 렌더링하지 않고 렌더링된 상태에서 각 요소의 display 속성만 변경하여 조건에 맞지 않는 데이터 숨기도록 처리

--- src/main.js
+++ src/main.js
@@ -1,38 +1,52 @@
-const $target = document.querySelector(".clothes");
-
-let state = { clothes: [], filteredClothes: []};
+let state = { clothes: [], key: '', value: '' };
 
 const setState = (newState) => {
   state = {...state, ...newState};
   render();
 }
 
-// 인수로 데이터를 넘겨받을 필요없이 밖에서 선언한 state 가져와서 사용하기. 왜?
 const render = () => {
   const { filteredClothes } = state;
-  $target.innerHTML = `${filteredClothes.map(({color, type, gender, size, imgUrl}) => {
-    return `<li class="cloth">
-            <img class="item__thumnail" src="${imgUrl}" alt="${color} ${type}">
-            <span class="item__description">${gender}, ${size}</span>
-        </li>`
-  }).join('')}`
+  const $target = document.querySelector(".clothes");
+  $target.innerHTML = `${filteredClothes.map((cloth) => createHTMLString(cloth)).join('')}`
 };
 
-// event delegation: closest 사용. 
-// https://ko.javascript.info/event-delegation
-document.querySelector("nav").addEventListener("click", e => {
-  const button = e.target.closest('BUTTON');
-  if (!button) {
+const createHTMLString = (cloth) => {
+  const { color, type, gender, size, imgUrl} = cloth;
+  return `<li class="cloth"  data-color=${color} data-type=${type}>
+            <img class="item__thumnail" src="${imgUrl}" alt="${color} ${type}"/>
+            <span class="item__description">${gender}, ${size}</span>
+        </li>`;
+}
+
+const setEventListener = () => {
+  const $logo = document.querySelector('.logo');
+  const $btns= document.querySelector('.btns');
+  
+  $logo.addEventListener('click', e => setState({ filteredClothes: state.clothes }));
+  $btns.addEventListener("click", e => onButtonClick(e));
+}
+
+const onButtonClick = (event) => {
+  const key = event.target.dataset.key;
+  const value = event.target.dataset.value;
+  if(!key || !value) {
     return;
   }
-  const filter = button.dataset.filter;
-  const selectedValue = button.dataset[filter];
-  const { clothes } = state;
-  //filter는 순수함수?
-  const filteredClothes = clothes.filter(cloth => cloth[filter] === selectedValue);
-  // render를 바로 호출하지 말고! 근데 왜??
-  setState({ filteredClothes });
-})
+  filterClothes(key, value);
+}
+
+// state 업데이트해서 리렌더링하는 대신 display 속성만 변경하기
+const filterClothes = (key, value) => {
+  const clothes = document.querySelectorAll('.cloth');
+  clothes.forEach(cloth => {
+    if (cloth.dataset[key] === value) {
+      cloth.classList.remove('invisible');
+    } else {
+      cloth.classList.add('invisible');
+    }
+  });
+}
 
 const loadClothes = async () => {
   try {
@@ -44,7 +58,7 @@ const loadClothes = async () => {
   }
 }
 
-//render();
+setEventListener();
 loadClothes(); // 최상위레벨에서 await 사용할 수 없음(node.js 최신버전은 가능) => 호출 후 다음 처리할 게 없으면 이렇게 호출해도 될듯
 
 
